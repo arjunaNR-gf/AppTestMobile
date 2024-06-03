9bbb8c54c26d151d2b312afcabcb2456f9711b44
arjunaNR-gf <arjun042896@gmail.com>
2024-06-03 19:47:57 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const[myname,setName] = useState({name:''})
+  useEffect(()=>{
+     axios.get("").then(res=>setName({name:res.data.name}))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{myname.name}</h2>
     </div>
   );
 }
