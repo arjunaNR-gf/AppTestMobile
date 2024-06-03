610a880d74cca3acba8ab8fd82fe77af4ad3d4e6
arjunaNR-gf <arjun042896@gmail.com>
2024-06-03 20:18:58 +0530

    added username api to test

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import axios from 'axios';
 function App() {
   const[myname,setName] = useState({name:''})
   useEffect(()=>{
-     axios.get("").then(res=>setName({name:res.data.name}))
-  })
+     axios.get("https://localhost:7141/UserName").then(res=>setName({name:res.data.name}))
+     .catch((error)=>{
+      console.log(error)
+     })
+  },[])
   return (
     <div className="App">
       <h2>{myname.name}</h2>
